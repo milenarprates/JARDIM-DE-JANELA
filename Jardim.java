cf3ef612cd7d3da4723a2ed4cfdd7ddbafb32ce6
Milena Prates <milenarprates@gmail.com>
2026-08-15 20:59:59 -0300

    Jardim

--- Jardim.java
+++ Jardim.java
@@ -1,3 +1,41 @@
+import java.util.ArrayList;
+
 public class Jardim {
 
+    private ArrayList<Planta> plantas;
+    private int capacidadeMaxima;
+    private boolean janelaAberta; //vai definir se as plantas recebem luz ou não
+
+    public Jardim() {
+        this.capacidadeMaxima = 6;
+        this.plantas = new ArrayList<>();
+        this.janelaAberta = false;
+    }
+
+    public void plantar(Planta planta) throws JardimCheioException {
+        if (plantas.size() >= capacidadeMaxima) {
+            throw new JardimCheioException(capacidadeMaxima);
+        }
+        plantas.add(planta);
+    }
+
+    public boolean isJanelaAberta() {
+        return janelaAberta;
+    }
+
+    public void setJanelaAberta(boolean janelaAberta) {
+        this.janelaAberta = janelaAberta;
+    }
+
+    public void atualizarTodasPlantas() {
+        for (Planta planta : plantas) {
+            planta.absorveAgua();
+
+            if (janelaAberta) {
+                planta.recebeLuz();
+            }
+
+            planta.crescer();
+        }
+    }
 }
--- Main.java
+++ Main.java
@@ -2,28 +2,8 @@ import java.util.Scanner;
 
 public class Main {
 
-    final static int REGA_LIRIO = 20;
-    final static int LUZ_LIRIO = 10;
-    final static int PERDA_AGUA_LIRIO = 5 * 1000;
-    final static int GANHO_LUZ_LIRIO = 5;
 
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-
-        System.out.println("Digite o nome da sua nova planta:");
-        String nomePlanta = sc.nextLine();
-
-        Especie lirioEspecie = new Especie(TipoPlanta.LIRIO,  REGA_LIRIO, LUZ_LIRIO, PERDA_AGUA_LIRIO, GANHO_LUZ_LIRIO);
-        Lirio lirio = new Lirio(nomePlanta, lirioEspecie);
-
-        lirio.exibeInfo();
-        lirio.serRegada();
-        lirio.serRegada();
-        lirio.serRegada();
-        lirio.crescer();
-        lirio.exibeInfo();
-
-        sc.close();
     }
 }
